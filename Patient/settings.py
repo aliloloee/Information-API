36ff3e9c7b717a415138a3d93640649825c16d6f
Ali loloee <aliloloee82@gmail.com>
2021-05-17 22:12:57 +0430

    host added

--- Patient/settings.py
+++ Patient/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'information-api.herokuapp.com']
 
 
 # Application definition
