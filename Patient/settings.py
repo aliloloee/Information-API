c864490c20804b3dbd53dc26a235e18497571329
Ali loloee <aliloloee82@gmail.com>
2021-05-17 22:27:17 +0430

    postgres db added

--- Patient/settings.py
+++ Patient/settings.py
@@ -77,8 +77,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('NAME'),
+        'USER' : config('USER'),
+        'PASSWORD' : config('PASSWORD'),
+        'HOST' : config('HOST'),
+        'PORT' : '5432'
     }
 }
 
